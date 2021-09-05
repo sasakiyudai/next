0eca6c8af965ea5e4bf0067c633eec2b8b0bc4ac
sasakiyudai <yudai14142@gmail.com>
2021-09-05 20:28:01 +0900

    done auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,18 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import Layout from '../components/layout/layout'
+import {Provider} from 'next-auth/client'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Head>
-        <meta name="viewport" content="width-device-width, initial-scale=1" />
-      </Head>
+    <Provider session={pageProps.session}>
+      <Layout>
+        <Head>
+          <meta name="viewport" content="width-device-width, initial-scale=1" />
+        </Head>
       <Component {...pageProps} />
-    </Layout>
+      </Layout>
+    </Provider>
   )
 }
 
